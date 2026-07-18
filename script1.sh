ff86dab861d8e75680ea450a9702cad02132a334
NIK501C <dell@DESKTOP-TTH266G.localdomain>
2026-07-18 15:43:03 +0000

    Add first script

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Nikki Kushwaha"
+
+echo "This file is for the learning process"
+
+echo "I will understand the concept and use it in my day to day devops journey"
+
+echo "Shell script is nothing but the linux commands in a script to automate the task and reduce the manual effort"
+
+echo "we are using /bash - #!/bin/bash is called shebang"
+
+echo "Thank you!!! Happy Learning"
