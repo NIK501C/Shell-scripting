d30a7a3d43d832c8f13679853c9ac9fa2a908aff
NIK501C <dell@DESKTOP-TTH266G.localdomain>
2026-07-18 16:18:27 +0000

    Add day 2 practice variables and read input

---
+++ day2/script2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# we use hash for a single line comment
+
+
+<< Nikki
+
+we use less than arrow symbol 
+with a name at start and end 
+for multi line comment
+
+Nikki
+
+# Variables
+
+
+fav_day="Friday"
+
+echo "My favourite day is $fav_day"
+
+# let's print the date command
+
+echo "Current date is $(date)"
+
+# take input from user
+
+echo "Enter your favourite day:"
+read favourite_day
+echo "Your favourite day is $favourite_day"
